a119879f9ac179868e6c4ad9b792ed8312711800
Sritaj <sritajp@gmail.com>
2021-04-25 11:42:20 +0530

    Minor text alignment on Header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <header className="app-header">
-        <div>Image Viewer</div>
+        <div className="text-header">Image Viewer</div>
       </header>
     );
   }
