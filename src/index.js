f6e9c9004c167492197786701bab30f867dcf141
Sritaj <sritajp@gmail.com>
2021-04-25 18:58:51 +0530

    Minute changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import Home from "./screens/home/Home";
 import Login from "./screens/login/Login";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(<Login />, document.getElementById("root"));
 registerServiceWorker();
